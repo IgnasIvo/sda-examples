9c2bd86dc5a202a9aff6049272f3a22a6c30c4af
Ignas Ivoška <ignasivo@gmail.com>
2021-02-27 22:04:15 +0200

    SDA-0: Initial commit

---
+++ src/main/java/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+/***
+ *
+ * @author Ignas Ivoska
+ *
+ */
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
