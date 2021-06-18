f736d9b7aa0cfc2d665ce4b1c00b465e3383d786
Ignas Ivoška <ignasivo@gmail.com>
2021-06-18 22:03:12 +0300

    add testing tools

---
+++ src/test/java/HelloWorldApplicationTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+/***
+ *
+ * @author Ignas Ivoska
+ *
+ */
+class HelloWorldApplicationTest {
+
+    @Test
+    void executes() {
+        HelloWorldApplication.main(new String[0]);
+    }
+
+}
